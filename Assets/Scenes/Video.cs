72cfdc9f0b4b0f6df89dcf3954b0b8630a6a84b2
Abhishek <agarg5@stanford.edu>
2016-03-19 16:07:41 -0700

    initial commit

---
+++ Assets/Scenes/Video.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Video : MonoBehaviour
+{
+
+    public MovieTexture movieclips;
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.mainTexture = movieclips;
+
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+
+        movieclips.Play();
+    }
+
+}
