949d31d49da27b3a78d74653ea1fd841a74b540d
Cathy Zhu <cathyxz@gmail.com>
2016-03-20 18:17:18 -0700

    dialogue text shows up correctly in VR

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -4,14 +4,25 @@ using System.Collections;
 public class DialogueManager : MonoBehaviour {
 	public GameObject canvas;
     public GameObject text;
+
 	// Use this for initialization
 	void Start () {
-		var t = Instantiate (text) as GameObject;
+        int xpos = Screen.width / 2;
+        int ypos = Screen.height / 3;
+        int width = Screen.width / 2;
+        int height = Screen.height / 8;
+        int spacing = height;
+
+
+        var t = Instantiate (text) as GameObject;
 		t.transform.SetParent (canvas.transform);
 		t.GetComponent<UnityEngine.UI.Text> ().text = alice1;
+        t.transform.position = new Vector3(0, -ypos, 0);
+
+        Debug.Log(t.GetComponent<UnityEngine.UI.Text>().rectTransform.position.ToString());
+
 
         startDialogue();
-
 	}
 	
 	// Update is called once per frame
@@ -22,8 +33,7 @@ public class DialogueManager : MonoBehaviour {
     IEnumerator startDialogue()
     {
         yield return new WaitForSeconds(3f);
-        Debug.Log("Got here");
-        
+        Debug.Log("DialogueManager - got here");
     }
 
 
