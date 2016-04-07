544a11d1c812acb5ed08642e315364a7f0221e2d
soye <sophixer@gmail.com>
2016-04-07 16:27:25 -0700

    added blowtorch as target, can now destroy blowtorch

---
+++ Assets/Scripts/Viewability.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Viewability : MonoBehaviour {
+
+    GameObject target;
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.GetComponent<Renderer>().IsVisibleFrom(Camera.main))
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Destroy(gameObject);
+                Debug.Log("We picked up the blow torch");
+            }
+        }
+    }
+
+}
