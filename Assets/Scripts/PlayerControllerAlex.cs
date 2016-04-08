18f63d1ba5a70c8155a3206c52751546de8c3ecd
alexm711 <alexm711@stanford.edu>
2016-04-08 00:18:01 -0700

    finished modifying controls

--- Assets/Scripts/PlayerControllerAlex.cs
+++ Assets/Scripts/PlayerControllerAlex.cs
@@ -8,16 +8,17 @@ public class PlayerControllerAlex : MonoBehaviour {
     public AudioClip forwardSound;
     public AudioClip backSound;
 
-    public float forceConstant = 5;
-    public float boost_multiplier = 10;
-    public float rollMagnitude = 100;
-    public float pitchMagnitude = 200;
+    public float force_constant = 5;
+    public float boost_magnitude = 3;
+    public float roll_magnitude = 50;
+    public float pitch_magnitude = 50;
+    public float yaw_magnitude = 50;
+
     public float braking_magnitude = 5;
-    public float yawMagnitude = 200;
-    public float brake_sleep_threshold = 2;
+    public float brake_sleep_threshold = 0.25f;
     public float camera_body_reorientation_time = 1;
     public float world_reorientation_time = 1;
-    public float vertical_boost_thrust = 100;
+    public float vertical_boost_thrust = 5;
     public float thrust_up_or_down_flag = 1;
     private Vector3 movementVector;
     private bool navigationDisabled = false;
@@ -46,43 +47,8 @@ public class PlayerControllerAlex : MonoBehaviour {
         navigationDisabled = false;
     }
 
-    //void move1()
-    //{
-    //    Vector3 forwardDirection = GameObject.FindObjectOfType<Camera>().transform.forward;
-    //    Vector3 rightDirection = GameObject.FindObjectOfType<Camera>().transform.right;
-
-
-    //    Vector3 force = forwardDirection * forceConstant;
-
-    //    if (Input.GetKey(KeyCode.UpArrow))
-    //    {
-    //        GetComponent<Rigidbody>().AddForce(force);
-    //    }
-    //    if (Input.GetAxis("Xbox_360_LeftJoystickX") != 0 || Input.GetAxis("Xbox_360_LeftJoystickY") != 0)
-    //    {
-    //        force = -Input.GetAxis("Xbox_360_LeftJoystickY") * forceConstant * forwardDirection + Input.GetAxis("Xbox_360_LeftJoystickX") * rightDirection * forceConstant;
-    //        GetComponent<Rigidbody>().AddForce(force);
-    //        playSound(force);
-    //    }
-
-    //    if (Input.GetKey(KeyCode.DownArrow))
-    //    {
-    //        force *= -1;
-    //        GetComponent<Rigidbody>().AddForce(force);
-    //    }
-
-    //    else if (Input.GetKey(KeyCode.Space) || Input.GetButtonDown("Xbox_360_A"))
-    //    {
-    //        GetComponent<Rigidbody>().Sleep();
-    //    }
-
-    //    else
-    //    {
-    //        SoundManager.instance.stopSoundEffect();
-    //    }
-    //}
     Vector3 applyBoost(Vector3 force){
-        if (Input.GetButton("Xbox_360_A")) { force *= boost_multiplier; }
+        if (Input.GetButton("Xbox_360_A")) { force *= boost_magnitude; }
         return force;
     }
 
@@ -94,12 +60,12 @@ public class PlayerControllerAlex : MonoBehaviour {
         Vector3 bodyRightDirection = GetComponent<Rigidbody>().transform.right;
         Vector3 bodyVerticalDirection = GetComponent<Rigidbody>().transform.up;
 
-        Vector3 force = bodyForwardDirection * forceConstant;
+        Vector3 force = bodyForwardDirection * force_constant;
 
         //Thrust left right forward backward
         if (Input.GetAxis("Xbox_360_LeftJoystickX") != 0 || Input.GetAxis("Xbox_360_LeftJoystickY") != 0)
         {
-            force = -Input.GetAxis("Xbox_360_LeftJoystickY") * forceConstant * bodyForwardDirection + Input.GetAxis("Xbox_360_LeftJoystickX") * bodyRightDirection * forceConstant;
+            force = -Input.GetAxis("Xbox_360_LeftJoystickY") * force_constant * bodyForwardDirection + Input.GetAxis("Xbox_360_LeftJoystickX") * bodyRightDirection * force_constant;
             force = applyBoost(force);
             GetComponent<Rigidbody>().AddForce(force);
             playSound(force);
@@ -107,24 +73,14 @@ public class PlayerControllerAlex : MonoBehaviour {
         //yaw, pitch 
         if (Input.GetAxis("Xbox_360_RightJoystickX") != 0 || Input.GetAxis("Xbox_360_RightJoystickY") != 0)
         {
-            //force = -Input.GetAxis("Xbox_360_LeftJoystickY") * forceConstant * bodyForwardDirection + Input.GetAxis("Xbox_360_LeftJoystickX") * bodyRightDirection * forceConstant;
-            //force = applyBoost(force);
-            float yawTurnSpeed = Input.GetAxis("Xbox_360_RightJoystickX");
-            float pitchTurnSpeed = Input.GetAxis("Xbox_360_RightJoystickY");
-            GetComponent<Rigidbody>().transform.Rotate(Vector3.up* yawTurnSpeed * 200* Time.deltaTime);
-            GetComponent<Rigidbody>().transform.Rotate(bodyRightDirection* pitchTurnSpeed * 200* Time.deltaTime);
+            float yawTurnSpeed = Input.GetAxis("Xbox_360_RightJoystickX")* 1;
+            float pitchTurnSpeed = Input.GetAxis("Xbox_360_RightJoystickY")* 1;
+            transform.Rotate(Vector3.up* yawTurnSpeed * yaw_magnitude * Time.deltaTime);
+            transform.Rotate(Vector3.right * pitchTurnSpeed * pitch_magnitude * Time.deltaTime);
             //playSound(force);
         }
         //Air-Braking
-        if (Input.GetButton("Xbox_360_B"))
-        {
-            Vector3 velocity = GetComponent<Rigidbody>().velocity;
-            if (velocity.magnitude < brake_sleep_threshold){
-                GetComponent<Rigidbody>().Sleep();
-            }else{
-                GetComponent<Rigidbody>().AddForce(applyBoost(velocity.normalized) * -forceConstant);
-            }
-        }
+        if (Input.GetButton("Xbox_360_B")) { GetComponent<Rigidbody>().AddForce(applyBoost(GetComponent<Rigidbody>().velocity.normalized) * -force_constant);}
 
         if (Input.GetButtonDown("Xbox_360_Y")){ thrust_up_or_down_flag *= -1;}
 
@@ -132,16 +88,13 @@ public class PlayerControllerAlex : MonoBehaviour {
         {
             int leftBoolInt = Input.GetButton("Xbox_360_LeftBumper") ? 1 : 0;
             int rightBoolInt = Input.GetButton("Xbox_360_RightBumper") ? 1 : 0;
-            float rollTurnSpeed = (leftBoolInt - rightBoolInt)*rollMagnitude;
-            transform.Rotate(bodyForwardDirection, rollTurnSpeed * Time.deltaTime);
-            GetComponent<Rigidbody>().transform.Rotate(bodyForwardDirection * 200* Time.deltaTime);
-
-
+            float rollTurnSpeed = (leftBoolInt - rightBoolInt)*roll_magnitude;
+            transform.Rotate(Vector3.forward * rollTurnSpeed * Time.deltaTime);
         }
 
-        if (Input.GetButton("Xbox_360_LeftStickClick"))
-        {
-            GetComponent<Rigidbody>().rotation = Quaternion.Slerp(GetComponent<Rigidbody>().rotation, Quaternion.identity,  0.02f);
+        if (Input.GetButton("Xbox_360_LeftStickClick")) {
+            float y= GetComponent<Rigidbody>().rotation.eulerAngles.y;
+            GetComponent<Rigidbody>().rotation = Quaternion.Slerp(GetComponent<Rigidbody>().rotation, Quaternion.Euler(0,y,0),  0.02f);
         }
         if (Input.GetButton("Xbox_360_RightStickClick"))
         {
@@ -149,7 +102,7 @@ public class PlayerControllerAlex : MonoBehaviour {
         }
         if (Input.GetAxis("Xbox_360_RightTrigger") != 0 && Input.GetAxis("Xbox_360_LeftTrigger") < 0.5)
         {
-            Vector3 vertical_force = bodyVerticalDirection * Input.GetAxis("Xbox_360_RightTrigger") * vertical_boost_thrust * thrust_up_or_down_flag*forceConstant;
+            Vector3 vertical_force = bodyVerticalDirection * Input.GetAxis("Xbox_360_RightTrigger") *  thrust_up_or_down_flag*force_constant;
             vertical_force = applyBoost(vertical_force);
             GetComponent<Rigidbody>().AddForce(vertical_force);
         }
