ae6877c9d28e64ac96d479b0733b6cc1b62f2d56
Alex <alexm711@stanford.edu>
2016-04-22 19:19:25 -0700

    tidied up audio

---
+++ Assets/Scripts/Wreckage/DestoryDoorOnConversationEnd.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryDoorOnConversationEnd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnConversationEnd(Transform actor){
+		GameObject Door = GameObject.Find ("Door");
+		Debug.Log(string.Format("Ending Conversatio1n"));
+		if (Door != null) {
+			Destroy (Door);
+			Debug.Log(string.Format("Ending Conversatio 2n"));
+			Destroy (this);
+		}
+	}
+}
+
