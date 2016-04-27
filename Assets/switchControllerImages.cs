7b7a9d0b416a2817bbf7818e8f83f6e790c50a30
Versatile <versatile@cs210.org>
2016-04-27 13:17:35 -0700

    changed to more obvious highlight color in settings panel, added fonts, changed to more readable fonts, fixed typos

--- Assets/switchControllerImages.cs
+++ Assets/switchControllerImages.cs
@@ -13,3 +13,34 @@ public class switchControllerImages : MonoBehaviour {
 	
 	}
 }
+//public AudioClip[] songs;
+//int currentSong = 0;
+//bool changeSong = false;
+//// Use this for initialization
+//void Start()
+//{
+
+//}
+
+//// Update is called once per frame
+//void Update()
+//{
+//    AudioSource audio = GetComponent<AudioSource>();
+//    if (audio.isPlaying == false || changeSong == true)
+//    {
+//        currentSong++;
+//        audio.clip = songs[currentSong % songs.Length];
+//        audio.Play();
+//        changeSong = false;
+//    }
+//}
+//public void ChangeSong()
+//{
+//    changeSong = true;
+//}
+//public void SetVolume(float val)
+//{
+//    Debug.Log("Volume val: " + val);
+
+//    GetComponent<AudioSource>().volume = val;
+//}
